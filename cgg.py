cc82e483250f5048f2f157e7bb273d0843613ddb
calcuis <113646141+calcuis@users.noreply.github.com>
2024-01-29 18:35:20 +0800

    Update cgg.py

--- cgg.py
+++ cgg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__version__="0.0.1"
+__version__="0.0.2"
 
 def __init__():
     import argparse
@@ -9,17 +9,12 @@ def __init__():
     parser.add_argument('tag', help='tag(s) available: c, cpp, g, gpp; or menu')
     args = parser.parse_args()
     if args.tag=="c":
-        # from .c import *
         from gguf_connector import c
     elif args.tag=="cpp":
-        # from .cpp import *
         from gguf_connector import cpp
     elif args.tag=="g":
-        # from .g import *
         from gguf_connector import g
     elif args.tag=="gpp":
-        # from .gpp import *
         from gguf_connector import gpp
     elif args.tag=="menu":
-        # from .gpp import *
         from gguf_connector import menu
